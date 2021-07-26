319f0200ffc2c86011b01e98e25f06cd5616219a
emuratalieva <emuratalieva@gmail.com>
2021-07-26 12:53:17 -0400

    Test case is all done

--- src/day1/ResettingPractice.java
+++ src/day1/ResettingPractice.java
@@ -6,5 +6,9 @@ public class ResettingPractice {
         //Test case part 1 is done
         //Test case part 2 is done
         // part 3 is done
+        //part 4 is done
+        //part 5 is done
+        // Mixed|Soft reset removes the commit from history but keep local cnages
+        // Did part 4 prt 5 and I am done in one commit
     }
 }
