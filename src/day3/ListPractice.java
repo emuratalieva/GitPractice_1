b6eec31c2a178f3fa9f01bd4f437ccb2c28ab644
emuratalieva <emuratalieva@gmail.com>
2021-07-29 14:35:17 -0400

    added 1 more line

--- src/day3/ListPractice.java
+++ src/day3/ListPractice.java
@@ -1,7 +1,11 @@
 package day3;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ListPractice {
     // new class has been created
     List<String> list = new ArrayList<>();
+    // added one more line of codes here
     
 }
