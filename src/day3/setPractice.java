e5b2588d384971ad015ca147c2d8f3feec1ab72e
emuratalieva <emuratalieva@gmail.com>
2021-07-29 14:45:48 -0400

    2nd commit for set class

--- src/day3/setPractice.java
+++ src/day3/setPractice.java
@@ -4,6 +4,7 @@ public class setPractice {
     public static void main(String[] args) {
 
         System.out.println("Set is collection type that stores unique item");
+        // added more lines here
 
     }
 }
