53e86c21aa4f9f4014d05274f6a474a82ffbc707
emuratalieva <emuratalieva@gmail.com>
2021-07-30 10:50:04 -0400

    master collection work done

--- src/day4/Conflict.java
+++ src/day4/Conflict.java
@@ -4,4 +4,5 @@ public class Conflict {
     // this is master branch content
 //this is final version
     // This is collection branch content
+    // this is the change collection2 branch does not know abt it
 }
